f86762b11df0b66c984d77712b3ed42bcf21782f
Muhammad Faisal <faisalmd25121999@gmail.com>
2026-02-21 21:25:54 +0530

    use json file on frontend

--- VoiceAI-backend/src/db/mongodb.py
+++ VoiceAI-backend/src/db/mongodb.py
@@ -1,10 +1,11 @@
 import os
+import certifi
 from pymongo import MongoClient
 
 MONGODB_URL = os.getenv('MONGODB_URL')
 DB_ENVIRONMENT = os.getenv('DB_ENVIRONMENT')
 
 def get_database():
-   client = MongoClient(MONGODB_URL)
+   client = MongoClient(MONGODB_URL, tlsCAFile=certifi.where())
 
    return client[str(DB_ENVIRONMENT)]
--- VoiceAI-backend/src/main.py
+++ VoiceAI-backend/src/main.py
@@ -1,27 +1,18 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
-from src.router import phone_agent_router
-from fastapi.responses import JSONResponse
-from contextlib import asynccontextmanager
-from fastapi.middleware.cors import CORSMiddleware
-from src.auth.session import authenticate_request
 
 load_dotenv()
+
+from fastapi import FastAPI
+from src.router import phone_agent_router
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
-
-@app.middleware("http")
-async def authentication_middleware(request: Request, call_next):
-    if "webhook" not in request.url.path:
-        if not authenticate_request(request):
-            return JSONResponse(status_code=401, content={"message": "Unable to complete request"})
-
-    response = await call_next(request)
-    return response 
 
 app.add_middleware(
     CORSMiddleware,
